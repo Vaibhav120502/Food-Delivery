f37e6ec91c24ca3ae35a432ce8e7e97f30a71d33
Vaibhav Singh <vaibhavair1@gmail.com>
2025-01-16 00:05:36 +0530

    commit5

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://abhayaj2121:Abhay2121@cluster0.nsa6cqa.mongodb.net/food-delivery').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://vaibhav2121:Vaibhav2121@cluster0.nsa6cqa.mongodb.net/food-delivery').then(()=>console.log("DB Connected"));
 }
